dbbf35fd92a1200a3ea127a2c17e14b63cf552d4
Bowen Ma <bma1024@bu.edu>
2022-09-19 18:45:15 -0400

    Added botometer function

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:url_launcher/url_launcher.dart';
 Future<void> main() async {
   // WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -158,18 +159,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController textEditingController = TextEditingController();
+  late TextEditingController _c;
+  //TextEditingController textEditingController = TextEditingController();
+  String _text = "initial";
   dynamic _id = "";
   dynamic _name = "";
   dynamic _username = "";
   
-  Future<void> readJson() async {
+  Future<void> readJson(String text) async {
     var headers = {
       'Access-Control-Allow-Origin': '*',
       'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAHbigwEAAAAAYLdcL0KXSTmyQJc%2FToCqDKoWSYg%3DL6Td4vHj1Q3MIvu7tjge6iyRYIwQOTXw0gCWaiS79ESHtPWrxj',
       'Cookie': 'guest_id=v1%3A166335990863918549'
     };
-    var request = http.Request('GET', Uri.parse('https://api.twitter.com/2/users/23083404'));
+    var request = http.Request('GET', Uri.parse('https://api.twitter.com/2/users/$text'));
     request.headers.addAll(headers);
 
     http.StreamedResponse response = await request.send();
@@ -213,6 +216,12 @@ class _MyHomePageState extends State<MyHomePage> {
   // }
 
   @override
+  void initState() {
+    _c = TextEditingController();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -224,9 +233,11 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Padding(
         padding: const EdgeInsets.all(25),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextField(
-              controller: textEditingController,
+              controller: _c,
+              decoration: const InputDecoration(hintText: "Search by id"),
               style: const TextStyle(
                 fontSize: 24,
                 color: Colors.black,
@@ -238,13 +249,18 @@ class _MyHomePageState extends State<MyHomePage> {
                 style: TextStyle(fontSize: 24),
               ),
               onPressed: () {
-                _sendDataToQueryUserbyIdScreen(context);
+                setState(() {
+                  _text = _c.text;
+                });
+                // Navigator.pop(context);
+                _sendDataToQueryUserbyIdScreen(context, _text);
               },
             ),
             ElevatedButton(
-              onPressed: readJson,
+              onPressed: () async {
+                await readJson(_text);
+              },
               child: const Text('Present Results'),
-              
             ),
             Text('id: $_id',
               textAlign: TextAlign.center,
@@ -281,8 +297,8 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-  void _sendDataToQueryUserbyIdScreen(BuildContext context) {
-    String textToSend = textEditingController.text;
+  void _sendDataToQueryUserbyIdScreen(BuildContext context, String text) {
+    String textToSend = text;
     Navigator.push(
         context,
         MaterialPageRoute(
@@ -291,24 +307,159 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class QueryUserbyIdScreen extends StatelessWidget {
+class QueryUserbyIdScreen extends StatefulWidget {
   final String text;
-
+  
   // receive data from the FirstScreen as a parameter
-  const QueryUserbyIdScreen({Key? key, required this.text}) : super(key: key);
+  const QueryUserbyIdScreen({super.key, required this.text});
+  @override
+  _QueryUserbyIdScreenState createState() {
+    return _QueryUserbyIdScreenState();
+  }
+}
 
+class _QueryUserbyIdScreenState extends State<QueryUserbyIdScreen> {
+  late TextEditingController _c;
+  //TextEditingController textEditingController = TextEditingController();
+  String _text = "initial";
+  dynamic _id = "";
+  dynamic _name = "";
+  dynamic _username = "";
+  var Data;
+  String url = 'http://10.0.2.2:5000/23083404';
+
+  Future<void> readJson(String text) async {
+    var headers = {
+      'Access-Control-Allow-Origin': '*',
+      'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAHbigwEAAAAAYLdcL0KXSTmyQJc%2FToCqDKoWSYg%3DL6Td4vHj1Q3MIvu7tjge6iyRYIwQOTXw0gCWaiS79ESHtPWrxj',
+      'Cookie': 'guest_id=v1%3A166335990863918549'
+    };
+    var request = http.Request('GET', Uri.parse('https://api.twitter.com/2/users/$text'));
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      var rawResponse = await response.stream.bytesToString();
+      //var rawResponse = await http.Response.fromStream(response);
+      var decodedResponse = json.decode(rawResponse);
+      setState(() {
+        _id = decodedResponse["data"]["id"];
+        _name = decodedResponse["data"]["name"];
+        _username = decodedResponse["data"]["username"];
+        
+      });
+      print(decodedResponse["data"]);
+    }
+    else {
+      print(response.reasonPhrase);
+      throw Exception("nononn");
+    }
+  }
+
+  Future ConvertUrl(url) async {
+    var request = http.Request('GET', Uri.parse(url));
+    var headers = {
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      // "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
+      "Access-Control-Allow-Headers": "Origin,content-type,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale, X-RapidAPI-Key, X-RapidAPI-Host",
+      'Referrer-Policy' : 'no-referrer-when-downgrade',
+      //'Accept': '*/*'
+    };
+    request.headers.addAll(headers);
+    http.StreamedResponse response = await request.send();
+    // var uri = Uri.https('10.0.2.2:5000', '/23083404');
+    // var response = await Client.get(uri);
+    // http.Response response = await http.get(Uri.parse(url),headers: headers);
+    // return response.body;
+    if (response.statusCode == 200) {
+      //var rawResponse = await response.stream.bytesToString();
+      var rawResponse = await http.Response.fromStream(response);
+      return rawResponse.body;
+    } else{
+      throw 'ConvertUrl request not successful';
+    }
+  }
+
+  @override
+  void initState() {
+    readJson(widget.text);
+    super.initState();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Second screen')),
+      appBar: AppBar(title: Text('Query User by id Result Page')),
       body: Center(
-        child: Text(
-          text,
-          style: TextStyle(fontSize: 24),
+        child: Column(children: [
+        ElevatedButton(
+              onPressed: () async {
+                await readJson(widget.text);
+              },
+              child: const Text('Present Results'),
+            ),
+        ElevatedButton(
+          onPressed: () async{
+            const url = 'https://twitter.com/i/flow/login';
+            if(await canLaunchUrl(Uri.parse(url))){
+              await launchUrl(Uri.parse(url));
+            } else {
+              throw 'Could not launch $url';
+            }
+          },
+          child: const Text('Open Twitter URL'),
         ),
+        Text('id: $_id',
+          textAlign: TextAlign.center,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(fontWeight: FontWeight.bold),),
+        Text('name: $_name',
+          textAlign: TextAlign.center,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(fontWeight: FontWeight.bold),),
+        Text('username: $_username',
+          textAlign: TextAlign.center,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(fontWeight: FontWeight.bold),),
+        TextField(
+          onChanged: (value) {
+            url = 'http://127.0.0.1:5000/$value';
+          },
+          decoration: const InputDecoration(
+            hintText: "Input id to check if user is real",
+            // suffixIcon: GestureDetector(onTap: ()async{
+            //   Data = await ConvertUrl(url);
+            //   var DecodedData = jsonDecode(Data);
+            //   setState(() {
+            //     _c.text = DecodedData['astroturf'];
+            //   });
+            // },),
+            ),
+          style: const TextStyle(
+            fontSize: 24,
+            color: Colors.black,
+          ),
+        ),
+        ElevatedButton(
+              onPressed: () async {
+                Data = await ConvertUrl(url);
+                //var DecodedData = jsonDecode(Data);
+                setState(() {
+                  _text = Data;
+                });
+              },
+              child: const Text('Click to check bot user'),
+            ),
+        Text('username: $_text, url = $url',
+          textAlign: TextAlign.center,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(fontWeight: FontWeight.bold),),
+        ],),
       ),
     );
   }
+  
 }
   
   // Widget build(BuildContext context) {
